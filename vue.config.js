53333293cf075b88688d9f83ca2a41ab86c2ac17
yuanwyj <1229102737@qq.com>
2019-06-10 15:07:54 +0800

    修改文件路径

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  publicPath: '/travis/',
+  outputDir: 'travis',
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+}
